46f0b13991f596b5079a2a6ac56a35975d26105c
Didi211 <miticd99@gmail.com>
2022-01-09 00:41:42 +0100

    neki apiji

--- Backend/routes/store.js
+++ Backend/routes/store.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const CreateRestoran = require('../controllers/storeController');
+const {
+    CreateStore,
+    GetStore,
+    GetAllStores
+} = require('../controllers/storeController');
 
-router.post('/', CreateRestoran);
+
+router.post('/create', CreateStore);
+router.get('/get/:id', GetStore);
+router.get('/all', GetAllStores);
 
 module.exports = router;
