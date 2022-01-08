df3a1b9b74ee728e0589231cd8ff0f7e6d9d3a2c
TeaMitic <tea.mitic@elfak.rs>
2022-01-08 22:17:14 +0100

    data.js, storeInfo komponenta, dbConnection.js

---
+++ Frontend/bolt-app/api-services/data.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import Api from '@/api-services/DBConnection.js'
+import router from "@/router"
+import cookies from 'vue-cookies'
+import { faWindows } from '@fortawesome/free-brands-svg-icons'
+
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state:{
+
+    },
+    getters:{
+
+    },
+    actions:{
+
+    },
+    mutations:{
+
+    }
+})
--- Frontend/bolt-app/src/pages/Homepage.vue
+++ Frontend/bolt-app/src/pages/Homepage.vue
@@ -17,25 +17,9 @@
                 <div class="container px-4 px-lg-5 mt-5">
                     <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                         <div class="col mb-5">
-                            <div class="card h-100">
-                                <!-- Product image-->
-                                <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
-                                <!-- Product details-->
-                                <div class="card-body p-4">
-                                    <div class="text-center">
-                                        <!-- Product name-->
-                                        <h5 class="fw-bolder">Fancy Product</h5>
-                                        <!-- Product price-->
-                                        $40.00 - $80.00
-                                    </div>
-                                </div>
-                                <!-- Product actions-->
-                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
-                                </div>
-                            </div>
+                            <StoreInfo />
                         </div>
-                        <div class="col mb-5">
+                        <div class="col mb-5 ">
                             <div class="card h-100">
                                 <!-- Sale badge-->
                                 <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
@@ -65,7 +49,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col mb-5">
+                        <div class="col mb-5 ">
                             <div class="card h-100">
                                 <!-- Sale badge-->
                                 <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
@@ -87,7 +71,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col mb-5">
+                        <div class="col mb-5 ">
                             <div class="card h-100">
                                 <!-- Product image-->
                                 <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
@@ -114,7 +98,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col mb-5">
+                        <div class="col mb-5 ">
                             <div class="card h-100">
                                 <!-- Sale badge-->
                                 <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
@@ -136,7 +120,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col mb-5">
+                        <div class="col mb-5 ">
                             <div class="card h-100">
                                 <!-- Product image-->
                                 <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
@@ -155,7 +139,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col mb-5">
+                        <div class="col mb-5 ">
                             <div class="card h-100">
                                 <!-- Sale badge-->
                                 <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
@@ -185,7 +169,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="col mb-5">
+                        <div class="col mb-5 ">
                             <div class="card h-100">
                                 <!-- Product image-->
                                 <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
@@ -227,16 +211,20 @@
 import { defineComponent } from '@vue/composition-api'
 import  Header  from '@/components/Header.vue'
 import  Footer  from '@/components/Footer.vue'
+import StoreInfo from '@/components/StoreInfoComponent.vue'
 
 export default defineComponent({
     name: "Homepage",
     components: {
         Header,
-        Footer
+        Footer,
+        StoreInfo
     }
 })
 </script>
 
 <style scoped>
-
+.visina{
+    height: 100px;
+}
 </style>
