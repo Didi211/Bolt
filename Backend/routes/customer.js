a30603a5f1cc8760e4493baa8686f4d134e6fc10
meelosh9 <91650382+meelosh9@users.noreply.github.com>
2022-01-10 14:22:40 +0100

    a
    a

--- Backend/routes/customer.js
+++ Backend/routes/customer.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const {CreateCustomer,
     GetPreviousOrders,
     ChangeLocation,
-    GetCustomer} = require('../controllers/customerController');
+    GetCustomer,GetCustomerByUsername} = require('../controllers/customerController');
 
 
 router.post('/',CreateCustomer); 
 router.get('/previousOrders/:id',GetPreviousOrders)
 router.put('/changeLocation/:id',ChangeLocation)
 router.get('/get/:id',GetCustomer)
+
+router.get('/get/username/:id',GetCustomerByUsername)
 module.exports = router;
