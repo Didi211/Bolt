fa61ce97534c78dfdd3c9ed9f3379d5c969bf2ed
Didi211 <miticd99@gmail.com>
2022-01-11 15:58:06 +0100

    redis config

---
+++ Backend/config/redis_config.js
@@ -0,0 +1,12 @@
+const redis = require('redis');
+
+const redis_client = redis.createClient();
+
+(async () => { 
+    redis_client.on('ready',() => console.log('Redis is ready.'));
+    redis_client.on('error',(err) => console.log('Redis Client Error',err));
+    await redis_client.connect();
+    
+})();
+
+module.exports = redis_client;
--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-//jwt
 const neo4j = require('./config/neo4j_config');
 
 
