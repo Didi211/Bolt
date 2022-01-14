bd03f0b6e090e7c1cade97c65befb6f2006630a5
TeaMitic <tea.mitic@elfak.rs>
2022-01-14 19:17:40 +0100

    deliverer

--- Frontend/bolt-app/src/api-services/data.js
+++ Frontend/bolt-app/src/api-services/data.js
@@ -364,6 +364,14 @@ export default new Vuex.Store({
                 console.log(err)
             })
         },
+        async getUnselectedOrdersDeliverer({commit}){
+            return await Api().get('/api/order/pendingDeliverer/').then(res=>{
+                commit('setUnselectedOrdersDeliverer',res.data)
+            }).catch(err=>{
+                console.log(err)
+            })
+
+        },
         async getPendingOrdersStore({commit}){
             return await Api().get('/api/order/pending/'+Vue.$cookies.get("id")).then(res=>{
                 const pendingOrders = res.data
@@ -415,7 +423,7 @@ export default new Vuex.Store({
         },
         primiObavestenjeMusteriji({commit}, message){
 
-            commit("postaviObavestenjeMusteriji", message.status)
+            commit("postaviObavestenjeMusteriji", message)
         }
     },
     mutations:{
