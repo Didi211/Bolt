d65f4e5c5003511a9d44a4acf9e980bdb859e250
TeaMitic <tea.mitic@elfak.rs>
2022-01-15 16:26:19 +0100

    vrv ce da bude merge

--- Frontend/bolt-app/src/App.vue
+++ Frontend/bolt-app/src/App.vue
@@ -23,80 +23,79 @@ export default {
     }
   },
   async created(){
+    console.log("uso u App.vue")
     this.korisnik.id = Vue.$cookies.get("id");
     this.korisnik.token = Vue.$cookies.get("token");
     this.korisnik.tip = Vue.$cookies.get("tip");
     
     if(this.korisnik != null && this.korisnik.id != null)
     {
+      console.log("uso u prvi if u App.vue")
       await this.$store.dispatch("postaviTip", this.korisnik.tip)
       await this.$store.dispatch("postaviToken", this.korisnik.token)
       
       await this.$store.dispatch("postaviOsobaID", this.korisnik.id)
       await this.$store.dispatch("getUserByID", this.korisnik.id)
+      
+      if(this.korisnik.tip == 'Customer'){
+        //otvori web socket za njegov port
+        const ws = new WebSocket("ws://localhost:3000/");
+        ws.onmessage = async (event) => {
+          let message = JSON.parse(event.data)
+          console.log(message)
+          if(message.customerID == this.korisnik.id){
+              await this.$store.dispatch("primiObavestenjeMusteriji", message)
+          }
+        }      
+      }
+      else if(this.korisnik.tip == 'Deliverer'){
+        //otvori web socket za njegov port
+        const ws = new WebSocket("ws://localhost:3001/");
+        ws.onmessage = async (event) => {
+          let message = JSON.parse(event.data)
+          console.log(message)
+          if(message.messageType == "Refresh"){
+            await this.$store.dispatch('getUnselectedOrdersDeliverer')
+          }
+          else if(message.messageType == 'OrderReady'){
+            console.log(message)
+            if(message.delivererID==this.korisnik.id){
+              await this.$store.dispatch("primiObavestenjeDeliverer", message)
+              await this.$store.dispatch('obPickUpOrderID', message.orderID)
+              await this.$store.dispatch('getAcceptedOrdersByDeliverer')
+
+            }
+          }
+          else if(message.messageType == 'newOrder'){
+            await this.$store.dispatch('getUnselectedOrdersDeliverer')
+          }        
+        }
+      }
+      else if(this.korisnik.tip == 'Store'){
+        //otvori web socket za njegov port
+        const ws = new WebSocket("ws://localhost:3002/");
+        ws.onmessage = async (event) => {
+          let message = JSON.parse(event.data)
+          console.log(message)
+          if(message.storeID == this.korisnik.id){
+            if(message.messageType=="NewOrder"){
+                await this.$store.dispatch("getPendingOrdersStore")
+            }
+            else if(message.messageType=="HasDeliverer"){
+              console.log("usao u else if za hasDeliverer")
+              
+                await this.$store.dispatch("changeDisabledOrderID", message.orderID)
+            }
+            else if(message.messageType=="PickedUp"){
+              console.log("usao u else if za PickedUp")
+                await this.$store.dispatch("getReadyOrdersStore")
+            }
+          }
+        }
+        // await this.$store.dispatch("postaviWebSocket", ws)
+      }
     }
     //mozda ovi if-ovi za ws treba da se ubace u ovaj gore if
-    if(this.korisnik.tip == 'Customer'){
-      //otvori web socket za njegov port
-      const ws = new WebSocket("ws://localhost:3000/");
-      ws.onmessage = async (event) => {
-        let message = JSON.parse(event.data)
-        console.log(message)
-        if(message.customerID == this.korisnik.id){
-            await this.$store.dispatch("primiObavestenjeMusteriji", message)
-        }
-      }
-
-      
-    }
-    else if(this.korisnik.tip == 'Deliverer'){
-      //otvori web socket za njegov port
-      const ws = new WebSocket("ws://localhost:3001/");
-      ws.onmessage = async (event) => {
-        let message = JSON.parse(event.data)
-        console.log(message)
-        if(message.messageType == "Refresh"){
-          await this.$store.dispatch('getUnselectedOrdersDeliverer')
-        }
-        else if(message.messageType == 'OrderReady'){
-          console.log(message)
-          if(message.delivererID==this.korisnik.id){
-            await this.$store.dispatch("primiObavestenjeDeliverer", message)
-            await this.$store.dispatch('obPickUpOrderID', message.orderID)
-            await this.$store.dispatch('getAcceptedOrdersByDeliverer')
-
-          }
-        }
-        else if(message.messageType == 'newOrder'){
-          await this.$store.dispatch('getUnselectedOrdersDeliverer')
-        }
-        
-      }
-
-    }
-    else if(this.korisnik.tip == 'Store'){
-      //otvori web socket za njegov port
-      const ws = new WebSocket("ws://localhost:3002/");
-      ws.onmessage = async (event) => {
-        let message = JSON.parse(event.data)
-        console.log(message)
-        if(message.storeID == this.korisnik.id){
-          if(message.messageType=="NewOrder"){
-              await this.$store.dispatch("getPendingOrdersStore")
-          }
-          else if(message.messageType=="HasDeliverer"){
-            console.log("usao u else if za hasDeliverer")
-            
-              await this.$store.dispatch("changeDisabledOrderID", message.orderID)
-          }
-          else if(message.messageType=="PickedUp"){
-            console.log("usao u else if za PickedUp")
-              await this.$store.dispatch("getReadyOrdersStore")
-          }
-        }
-      }
-      // await this.$store.dispatch("postaviWebSocket", ws)
-    }
 
 
   }
