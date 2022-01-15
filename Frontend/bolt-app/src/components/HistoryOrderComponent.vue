2200226f07337d9aa1223b9104775f45e6b35d67
TeaMitic <tea.mitic@elfak.rs>
2022-01-15 20:00:22 +0100

    neke promene

--- Frontend/bolt-app/src/components/HistoryOrderComponent.vue
+++ Frontend/bolt-app/src/components/HistoryOrderComponent.vue
@@ -3,6 +3,7 @@
             <div class="card-body p-4 order">
                 <div class="text-center">
                     <div class="row">
+                        <h5>{{narudzbina.store[0].name}}</h5>
                         <li v-for="m in narudzbina.meals" :key="m.mealID"> {{m.name}} {{m.price}} din</li>
                         <div class="col-md-12">
                             <p>Adresa:</p>
@@ -13,7 +14,7 @@
                     <div class="row">
                     <input type="textarea" v-model="narudzbina.order.note" id="note" class="m-3 note">
                     </div>                   
-                    <button type="button" class="btn btn-dark" @click="orderMeals" >Naruci ponovo</button>
+                    <button type="button" class="btn btn-dark" @click="orderMeals">Naruci ponovo</button>
                     <!-- @click="order" -->
                 </div>
             </div>
@@ -43,7 +44,8 @@ export default defineComponent({
             onAddress: "",
             note: "",
             meals: [],
-            uuid: ""
+            uuid: "",
+            storeID: null
         }
       }
   },
@@ -62,16 +64,17 @@ export default defineComponent({
     //     this.$store.dispatch('changeCustomerLocation', this.loc)
     // },
     orderMeals(){
-        this.orderMeal.onAddress=document.getElementById("location").value
-        this.orderMeal.note=document.getElementById("note").value
+        this.orderMeal.onAddress=this.$props.narudzbina.order.onAddress
+        this.orderMeal.note=this.$props.narudzbina.order.note
         const jelaId =[]
         
-        for (let i =0; i< this.$props.narudzbina.meals.lenght;i++){
+        for (let i =0; i< this.$props.narudzbina.meals.length;i++){
             jelaId.push(this.$props.narudzbina.meals[i].mealID)
         }
 
         this.orderMeal.meals=jelaId
         this.orderMeal.uuid=this.customer.uuid
+        this.orderMeal.storeID=this.$props.narudzbina.store[0].uuid
         console.log("ovo je iz order u store menu")
         console.log(this.orderMeal)
         this.$store.dispatch('orderMeal', this.orderMeal)
